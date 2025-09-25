535704397a1a84ef5e86435b73883004d2705c99
Trent Garner <trentongarner.13@gmail.com>
2025-09-24 23:33:09 -0600

    Theme added

---
+++ app/assets/javascripts/theme/vendor_bundle.js
@@ -0,0 +1,17 @@
+//= require theme/vendor/imagesloaded/imagesloaded.pkgd.min
+//= require theme/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min
+//= require theme/vendor/simplebar/dist/simplebar.min
+//= require theme/vendor/aos/dist/aos
+//= require theme/vendor/swiper/swiper-bundle.min
+//= require theme/vendor/shufflejs/dist/shuffle.min
+//= require theme/vendor/flatpickr/dist/flatpickr
+//= require theme/vendor/nouislider/dist/nouislider.min
+//= require theme/vendor/img-comparison-slider/dist/index
+//= require theme/vendor/lightgallery/lightgallery.min
+//= require theme/vendor/lightgallery/plugins/zoom/lg-zoom.min
+//= require theme/vendor/lightgallery/plugins/video/lg-video.min
+//= require theme/vendor/leaflet/dist/leaflet
+//= require theme/vendor/jarallax/dist/jarallax.min
+//= require theme/vendor/parallax-js/dist/parallax.min
+//= require theme/vendor/timezz/dist/timezz
+
